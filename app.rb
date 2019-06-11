0f4fff443c93e56e05a87139ab8762d23d9f03ee
phazel <courtney.p.hazel@gmail.com>
2019-06-11 19:10:41 +1000

    Breed hardcoded values with one gene

---
+++ app.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+require_relative 'genetics'
+
+class App
+  puts Genetics.process
+end
---
+++ genetics.rb
@@ -0,0 +1,8 @@
+class Genetics
+  VALUE1 = ['B', 'b']
+  VALUE2 = ['B', 'b']
+
+  def self.process
+    [VALUE1.sample, VALUE2.sample]
+  end
+end
